eb9ebcec84f07cac9f6750e78291c6a10a4cb514
henrybuchi1 <chukwubuchi0@gmail.com>
2022-09-22 11:15:51 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,25 +3,49 @@
 
 
 /**
-
- * _strncpy - copy specific bytes of characters from one string to another
-
  *
-
- * @dest: the string to be copied to
-
- * @src: the string to copy
-
- * @n: number of bytes to copy
-
+ *  * _strncpy - copy specific bytes of characters from one string to another
  *
-
- * Return: the destination string
-
- */
+ *   *
+ *
+ *    * @dest: the string to be copied to
+ *
+ *     * @src: the string to copy
+ *
+ *      * @n: number of bytes to copy
+ *
+ *       *
+ *
+ *        * Return: the destination string
+ *
+ *         */
 
 char *_strncpy(char *dest, char *src, int n)
 
 {
 
-int i;
+		int i;
+
+
+
+			for (i = 0; src[i] != '\0' && i < n; ++i)
+
+					{
+
+								dest[i] = src[i];
+
+									}
+
+				while (i < n)
+
+						{
+
+									dest[i] = 0;
+
+											++i;
+
+												}
+
+					return (dest);
+
+}
